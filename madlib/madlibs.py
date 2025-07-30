446375026c62fd282531e68c33e12631e0243bb8
Ramla Eman <ramla.eman.ra@gmail.com>
2025-07-30 16:18:46 +0500

    created project name Madlib

---
+++ madlib/madlibs.py
@@ -0,0 +1,6 @@
+name = input("Name: ")
+place = input("Place: ")
+activity = input("Activity: ")
+
+madlib = f"One day {name} went to the {place} and started {activity} like nobody was watching!"
+print(madlib)
