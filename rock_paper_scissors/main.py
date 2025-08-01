42de80947b5ebedec3f21696e3ba161382bd3f8d
Ramla Eman <ramla.eman.ra@gmail.com>
2025-08-01 14:41:23 +0500

    enhance the code of rock paper scisors

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -1,20 +1,53 @@
 import random
 
-def play():
-    user = input(f"What's your choice? 'r' for rock, 'p' for Paper 's' for Scissors\n")
-    computer = random.choice(['r', 'p', 's'])
+ROCK = 'r'
+PAPER = 'p'
+SCISSORS = 's'
+emojis = {ROCK: '✊', PAPER: '📃', SCISSORS: '✂'}
+choices = tuple(emojis.keys())
 
+
+def get_user_choice():
+    while True:
+        user = input(
+            f"What's your choice? {ROCK} for Rock, {PAPER} for Paper, {SCISSORS} for Scissors: ").lower()
+
+        if user in choices:
+            return user
+        else:
+            print('Invalid Choice')
+
+
+def show_choices(user, computer):
+    print(f'you choose {emojis[user]}')
+    print(f'computer choose {emojis[computer]}')
+
+
+def is_win(user, computer):
     if user == computer:
-        return f'It\'s a Draw ! Try Again'
-    
-    if is_win(user, computer):
-        return f'You Won! Computer chose {computer}'
+        return f"It's a Draw!"
+    elif (
+        (user == ROCK and computer == SCISSORS) or
+        (user == SCISSORS and computer == PAPER) or
+        (user == PAPER and computer == ROCK)
+    ):
+        print(f"You Won!")
     else:
-        return f'Computer Won! Computer chose {computer}'
+        print('You Lose')
 
 
-def is_win(player, opponent):
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
-        return True
+def play():
+    while True:
+        user = get_user_choice()
 
-print(play())
+        computer = random.choice(choices)
+
+        show_choices(user, computer)
+        is_win(user, computer)
+
+        should_continue = input('continue (y/n): ').lower()
+        if should_continue == 'n':
+            break
+
+
+play()
