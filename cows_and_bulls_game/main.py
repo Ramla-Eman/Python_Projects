61e983c9d8a86455d56995586e802490f3123b93
Ramla Eman <ramla.eman.ra@gmail.com>
2025-08-09 22:12:55 +0500

    write the of cows_and_bulls game

---
+++ cows_and_bulls_game/main.py
@@ -0,0 +1,32 @@
+import random
+
+def generate_secret():
+    digits = list(range(10))
+    random.shuffle(digits)
+    return ''.join([str(digit)for digit in digits[:4]])
+
+def calcultae_cows_and_bulls(secret,guess):
+    bulls = sum([1 for i in range(4) if guess[i] == secret[i]])
+    cows = sum([1 for i in range(4) if guess[i] in secret]) - bulls
+
+    return cows, bulls
+
+def main():
+    secret = generate_secret()
+    print('I have generate a 4-digits number with unique digits. Try to guess it!')
+
+    while True:
+        guess = input('Guess: ')
+        if len(guess) == 4 and guess.isdigit() and len(set(guess)) == 4:
+            cows, bulls = calcultae_cows_and_bulls(secret,guess) 
+            print(f'{cows} cows, {bulls} bulls')
+
+            if bulls == 4:
+                print('Congratulations you guessed the correct Number')
+                break
+        else:
+            print('Invalid Guess. Please enter a 4-digit number with unique digits.')
+
+
+if __name__ == "__main__":
+    main()
